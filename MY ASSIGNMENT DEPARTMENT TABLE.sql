a5184f855b66302dd12f0e8c0fe44bbac09c2062
Ecoplex82 <emmanuelchetachibright@gmail.com>
2025-02-08 19:14:42 +0100

    Add files via upload

---
+++ MY ASSIGNMENT DEPARTMENT TABLE.sql
@@ -0,0 +1,5 @@
+CREATE TABLE department(
+dept_id INT PRIMARY KEY NOT NULL,
+dept_name VARCHAR (20) NOT NULL,
+manager VARCHAR (20) NOT NULL,
+);
---
+++ my project table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE project(
+proj_id INT PRIMARY KEY NOT NULL,
+proj_name VARCHAR (50) NOT NULL,
+proj_decription VARCHAR (100) NOT NULL,
+proj_budget VARCHAR (30) NOT NULL,
+proj_manager VARCHAR (30) NOT NULL,
+start_date VARCHAR (30) NOT NULL,
+end_date VARCHAR (30) NOT NULL,
+project_status VARCHAR (30) NOT NULL,
+);
+
+SELECT * FROM project;
+
+INSERT INTO project(proj_budget,proj_decription,proj_id,proj_name,project_status,start_date,end_date, proj_manager)
+VALUES ('$1000', 'Street Light Installation', 201, 'Solar Installation 1', 'In Progress', '01-02-2025', '23-03-2025', 'Emmyrex'),
+('$200','Home Appliances Maintenance', 199, 'Maintenance 1', 'Under Review', '02-02-2025', '10-02-2025', 'Dantel'),
+('$100', 'House Wiring Maintenance', 202, 'Maintenance 2', 'Back log', '01-01-2025', '20-01-2025', 'Jude'),
+('$500', 'Lie Dictator App Development', 203, 'App Development', 'Pending', '05-02-2025', '10-10-2025', 'Ecoplex-world'),
+('$300', 'Reconstruction Of Enugu-Abakaliki express Road', 204, 'road Construction', 'Approved','02-01-2024', '05-03-2025', 'Bright');
